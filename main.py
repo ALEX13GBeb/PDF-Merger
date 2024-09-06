819b35484589ea5ebfcd3457ac565fe438332ae8
ALEX13GBeb <167245668+ALEX13GBeb@users.noreply.github.com>
2024-09-06 10:56:51 +0300

    Added .idea and __pycache__ to .gitignore

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-from modules import merger_pdf
-
-pdf_files=[path for path in os.listdir() if path.endswith(".pdf")]
-
-merger_pdf(pdf_files)
-
-print(f"Succesfully merged {len(pdf_files)} pdfs into output_pdf")
